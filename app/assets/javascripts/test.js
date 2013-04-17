93d7490d7256775632cd163827c03a17e4488883
jrlapayne <jrlapayne@gmail.com>
2013-04-17 16:53:41 -0700

    add to my repo

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,17 +1,26 @@
-$(function(){
-    var menuStatus = false;
- 
-    $("a.showMenu").click(function(){
-		if(menuStatus == false){
-			$("#menu").show(function(){menuStatus = true})
-			$("#home").css('width', '10px');
-			}
-		if(menuStatus == true){
-			$("#menu").hide(function(){menuStatus = false})
-			$("#home").css('width', '400px');
-		}
-    });
- 
-  
- 
+$(document).ready(function(){
+    var ex1running = false;
+    var ex2running = false;
+
+    $('#meter-ex1').ready(function(){ 
+	  if(!ex1running){
+        ex1running = true;
+        var $this = $(this);
+        var count = 100;
+        var inter = null;
+		var points = 1000
+        function run(){
+			count = count - 10;
+            points = points - 100;
+            $this.find('.meter-value').css('width', count+"%");
+            $this.find('.meter-text').text(points);
+            if(count == 0){
+                clearInterval(inter);
+				ex1running = false;
+       		}
+        }
+		inter = setInterval(run, 1000);
+	}
+});
+
 });
